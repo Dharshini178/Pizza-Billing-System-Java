6b3b9ac86efbc8614a73c39d1b4f0a7db7282cd7
priyu67 <priyadharshini7785@gmail.com>
2026-01-02 12:26:08 +0530

    Initial commit - Pizza Billing System

---
+++ src/com/pizzabilling/DeluxPizza.java
@@ -0,0 +1,18 @@
+package com.pizzabilling;
+
+public class DeluxPizza extends Pizza{
+
+	DeluxPizza(Boolean veg) {
+		super(veg);
+		super.addExtraCheese();
+		super.addExtraToppings();
+		
+	}
+	
+	public void addExtraCheese() {}
+	
+	public void addExtraToppings() {
+		}
+	}
+	
+	
---
+++ src/com/pizzabilling/Main.java
@@ -0,0 +1,14 @@
+package com.pizzabilling;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		DeluxPizza dp = new DeluxPizza(false);
+		dp.takeAway();
+		dp.getBill();
+		
+	
+	}
+
+}
